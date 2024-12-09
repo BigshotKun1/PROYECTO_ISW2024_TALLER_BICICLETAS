b86d6e93eedeafbb1afb7edfb5ec5b9161628f10
BigshotKun1 <vicente.castillo2001@alumnos.ubiobio.cl>
2024-12-09 12:33:38 -0300

    UPDATE FRONT COMPONENTS- HOOKS- SERVICES

--- frontend/src/services/PedidoReparacion.service.js
+++ frontend/src/services/PedidoReparacion.service.js
@@ -2,19 +2,19 @@ import axios from 'axios';
 import { formatUserData } from '@helpers/formatData.js';
 
 
-// Función para crear un nuevo pedido de reparación
+
 export const createPedidoReparacion = async (newPedido) => {
   try {
-    // Realizamos la solicitud POST para crear un nuevo pedido
+ 
     const response = await axios.post('/pedidoReparacion', newPedido);
     
-    // Si la respuesta es exitosa, retornamos los datos
+
     return {
       status: "Success",
       data: response.data,
     };
   } catch (error) {
-    // Si ocurre un error, capturamos y retornamos el mensaje de error
+   
     console.error("Error al crear el pedido:", error.response ? error.response.data : error);
     return {
       status: "Error",
@@ -23,7 +23,7 @@ export const createPedidoReparacion = async (newPedido) => {
   }
 };
 
-// Función para obtener todos los pedidos de reparación (si la necesitas en algún momento)
+
 export const fetchPedidosReparacion = async () => {
   try {
     const response = await axios.get('/pedidoReparacion/all');
@@ -40,7 +40,7 @@ export const fetchPedidosReparacion = async () => {
   }
 };
 
-// Función para obtener un pedido específico por su ID (si la necesitas en algún momento)
+
 export const fetchPedidoReparacionById = async (id) => {
   try {
     const response = await axios.get('/pedidoReparacion/:id_PedidoReparacion'+ id);
