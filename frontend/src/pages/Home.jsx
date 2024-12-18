d7e35f062e3fa2f313d1824f2347e80b147e57d9
EmilioAlarconGomez <emilio.alarcon.gomez@gmail.com>
2024-12-18 07:12:06 -0300

    fix home, navbar

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -52,7 +52,7 @@ export default Home;
 
 */
 
-import '../styles/home-background.css';  // Ajusta la ruta según corresponda
+import '../styles/home-background.css';
 import { useEffect, useState } from 'react';
 
 const Home = () => {
@@ -72,12 +72,12 @@ const Home = () => {
         <header style={{ textAlign: 'center', marginBottom: '30px', marginTop: '100px' }}>
           <h1>TALLER BICICLETAS UBB</h1>
           <div className="user-info">
-    <span className="user-name">{usuario?.nombreCompleto}</span>
-    <span className="user-role">{usuario?.rol}</span>
-  </div>
+            <span className="user-name">{usuario?.nombreCompleto}</span>
+            <span className="user-role">{usuario?.rol}</span>
+          </div>
         </header>
 
-        <main style={{ marginTop: '30px' }}>
+        <main>
           <h2>Panel de {usuario ? usuario.rol : 'Usuario'}</h2>
           {usuario?.rol === 'mecánico' && <p>Revisar pedidos en espera o en reparación.</p>}
           {usuario?.rol === 'vendedor' && <p>Gestión de clientes y productos.</p>}
